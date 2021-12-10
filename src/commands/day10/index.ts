58c1e195af6f4d6bae503d8ab9ba7a6ac945fb98
Rory Claasen <rory@xbox.com>
2021-12-10 09:03:24 +0000

    New: Adds day 10 completed challenge

---
+++ src/commands/day10/index.ts
@@ -0,0 +1,90 @@
+import AdventCommand from '../../base';
+import { parseFile, splitLines } from '../../utils';
+
+type Input = string[];
+export default class Day10Challenge extends AdventCommand<Input> {
+    private readonly opening = ['(', '[', '{', '<'];
+    private readonly closing = [')', ']', '}', '>'];
+
+    static aliases = ['day:10'];
+
+    protected async parseInput(test: boolean): Promise<Input> {
+        const data = await parseFile(test ? 'testinput' : 'input', __dirname);
+        return splitLines(data);
+    }
+
+    private checkLineSyntax(line: string, onError?: (char: string) => void): string[] {
+        const syntax = [...line];
+        const stack: string[] = [];
+        for (const char of syntax) {
+            if (this.opening.includes(char)) {
+                stack.push(char);
+            } else if (this.closing.includes(char)) {
+                if (stack[stack.length - 1] === this.opening[this.closing.indexOf(char)]) {
+                    stack.pop();
+                } else if (onError) {
+                    onError(char);
+                    break;
+                }
+            } else {
+                throw new Error(`Invalid character '${char}'`);
+            }
+        }
+
+        return stack;
+    }
+
+    protected part1(input: Input): number {
+        let totalError = 0;
+        for (const line of input) {
+            this.checkLineSyntax(line, (char) => {
+                switch (char) {
+                    case ')':
+                        totalError += 3;
+                        break;
+                    case ']':
+                        totalError += 57;
+                        break;
+                    case '}':
+                        totalError += 1197;
+                        break;
+                    case '>':
+                        totalError += 25_137;
+                        break;
+                }
+            });
+        }
+
+        return totalError;
+    }
+
+    protected part2(input: Input): number {
+        const incompleteInputLines: string[] = [];
+        for (const line of input) {
+            let error = false;
+
+            this.checkLineSyntax(line, () => {
+                error = true;
+            });
+
+            if (!error) {
+                incompleteInputLines.push(line);
+            }
+        }
+
+        const scores: number[] = [];
+        for (const line of incompleteInputLines) {
+            const stack = this.checkLineSyntax(line);
+
+            let score = 0;
+            for (const char of stack.reverse()) {
+                score *= 5;
+                score += this.opening.indexOf(char) + 1;
+            }
+
+            scores.push(score);
+        }
+
+        return scores.sort((a, b) => a - b)[Math.floor(scores.length / 2)];
+    }
+}
---
+++ test/commands/day10.test.ts
@@ -0,0 +1,29 @@
+import { expect, test } from '@oclif/test';
+
+import { answerString } from '../helpers/answer';
+
+describe('day10', () => {
+    const PART1_ANSWER = answerString(1, 26_397);
+    const PART2_ANSWER = answerString(2, 288_957);
+
+    test.stdout()
+        .command(['day10', '--test'])
+        .it('Calculates all answers correctly', (ctx) => {
+            expect(ctx.stdout).to.contain(PART1_ANSWER, 'Part 1');
+            expect(ctx.stdout).to.contain(PART2_ANSWER, 'Part 2');
+        });
+
+    test.stdout()
+        .command(['day10', '--test', '--part=1'])
+        .it('Calculates only part 1 answer correctly', (ctx) => {
+            expect(ctx.stdout).to.contain(PART1_ANSWER, 'Part 1');
+            expect(ctx.stdout).to.not.contain(PART2_ANSWER, 'Part 2');
+        });
+
+    test.stdout()
+        .command(['day10', '--test', '--part=2'])
+        .it('Calculates only part 2 answer correctly', (ctx) => {
+            expect(ctx.stdout).to.not.contain(PART1_ANSWER, 'Part 1');
+            expect(ctx.stdout).to.contain(PART2_ANSWER, 'Part 2');
+        });
+});
