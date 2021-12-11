211d8dc9996b57c379634c28c0d5edc41f4aa1f2
Rory Claasen <rory@xbox.com>
2021-12-11 14:10:01 +0000

    Chore: Refactors day11

--- src/commands/day11/index.ts
+++ src/commands/day11/index.ts
@@ -18,7 +18,7 @@ export default class Day11Challenge extends AdventCommand<Input> {
         return splitLines(data).map((line) => [...line].map(toNumber).map((n) => ({ energy: n, flashed: false })));
     }
 
-    private updateNeiboghors(input: Input, x: number, y: number): void {
+    private incrementNeiboghors(input: Input, x: number, y: number): void {
         if (x > 0) input[y][x - 1].energy++;
         if (x < input[y].length - 1) input[y][x + 1].energy++;
         if (y > 0) input[y - 1][x].energy++;
@@ -30,33 +30,39 @@ export default class Day11Challenge extends AdventCommand<Input> {
         if (x < input[y].length - 1 && y < input.length - 1) input[y + 1][x + 1].energy++;
     }
 
-    protected part1(input: Input): number {
-        let flashes = 0;
-        for (let i = 0; i < 100; i++) {
-            for (const row of input) for (const oct of row) oct.energy++;
+    private processItteration(input: Input, onFlash?: () => void): void {
+        for (const row of input) {
+            for (const oct of row) {
+                if (oct.energy > 9) {
+                    oct.energy = 0;
+                    oct.flashed = false;
+                }
+            }
+        }
 
-            do {
-                for (let y = 0; y < input.length; y++) {
-                    for (let x = 0; x < input[y].length; x++) {
-                        const octopus = input[y][x];
+        for (const row of input) for (const oct of row) oct.energy++;
+
+        do {
+            for (let y = 0; y < input.length; y++) {
+                for (let x = 0; x < input[y].length; x++) {
+                    const octopus = input[y][x];
+                    if (octopus.energy > 9 && !octopus.flashed) {
+                        this.incrementNeiboghors(input, x, y);
+                        octopus.flashed = true;
                         // eslint-disable-next-line max-depth
-                        if (octopus.energy > 9 && !octopus.flashed) {
-                            flashes++;
-                            this.updateNeiboghors(input, x, y);
-                            octopus.flashed = true;
+                        if (onFlash) {
+                            onFlash();
                         }
                     }
                 }
-            } while (input.flat().some((oct) => oct.energy > 9 && !oct.flashed));
-
-            for (const row of input) {
-                for (const oct of row) {
-                    if (oct.energy > 9) {
-                        oct.energy = 0;
-                        oct.flashed = false;
-                    }
-                }
             }
+        } while (input.flat().some((oct) => oct.energy > 9 && !oct.flashed));
+    }
+
+    protected part1(input: Input): number {
+        let flashes = 0;
+        for (let i = 0; i < 100; i++) {
+            this.processItteration(input, () => flashes++);
         }
 
         return flashes;
@@ -65,30 +71,8 @@ export default class Day11Challenge extends AdventCommand<Input> {
     protected part2(input: Input): number {
         let step = 0;
         do {
-            for (const row of input) {
-                for (const oct of row) {
-                    if (oct.energy > 9) {
-                        oct.energy = 0;
-                        oct.flashed = false;
-                    }
-                }
-            }
-
             step++;
-            for (const row of input) for (const oct of row) oct.energy++;
-
-            do {
-                for (let y = 0; y < input.length; y++) {
-                    for (let x = 0; x < input[y].length; x++) {
-                        const octopus = input[y][x];
-                        // eslint-disable-next-line max-depth
-                        if (octopus.energy > 9 && !octopus.flashed) {
-                            this.updateNeiboghors(input, x, y);
-                            octopus.flashed = true;
-                        }
-                    }
-                }
-            } while (input.flat().some((oct) => oct.energy > 9 && !oct.flashed));
+            this.processItteration(input);
         } while (input.flat().some((oct) => !oct.flashed));
 
         return step;
