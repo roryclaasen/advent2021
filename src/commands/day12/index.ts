608c5c89ce7d8d72a6bbd9e45eacf7dde5f55d4d
Rory Claasen <rory@xbox.com>
2021-12-12 11:43:30 +0000

    Update: Refactors day12 into one method

--- src/commands/day12/index.ts
+++ src/commands/day12/index.ts
@@ -5,7 +5,7 @@ type Node = {
     links: Set<string>;
     isBig: boolean;
     id: string;
-    leaf: boolean;
+    isLeaf: boolean;
 };
 
 type Input = Map<string, Node>;
@@ -18,7 +18,7 @@ export default class Day12Challenge extends AdventCommand<Input> {
         const connections = new Map<string, Node>();
 
         const mapConnections = (a: string, b: string) => {
-            const objectA = connections.has(a) ? connections.get(a) : { links: new Set<string>(), isBig: /[A-Z]/.test(a), id: a, leaf: ['start', 'end'].includes(a) };
+            const objectA = connections.has(a) ? connections.get(a) : { links: new Set<string>(), isBig: /[A-Z]/.test(a), id: a, isLeaf: ['start', 'end'].includes(a) };
             objectA.links.add(b);
 
             connections.set(a, objectA);
@@ -34,34 +34,12 @@ export default class Day12Challenge extends AdventCommand<Input> {
         return connections;
     }
 
-    private walk1(input: Input, node: Node, currentPath: string[] = [], visited = new Set<string>()): number {
-        let paths = 0;
-        const newCurrentPath = [...currentPath];
-
-        if (visited.has(node.id)) {
-            return paths;
-        }
-
-        if (!node.isBig) {
-            visited.add(node.id);
-        }
-
-        newCurrentPath.push(node.id);
-
-        for (const link of node.links) {
-            paths += link === 'end' ? 1 : this.walk1(input, input.get(link), newCurrentPath, new Set(visited));
-        }
-
-        return paths;
-    }
-
     // eslint-disable-next-line max-params
-    private walk2(input: Input, node: Node, currentPath: string[] = [], visited = new Set<string>(), flag = false): number {
+    private walk(input: Input, node: Node, exitCondidtion: (node: Node, flag: boolean) => boolean, currentPath: string[] = [], visited = new Set<string>(), flag = false): number {
         let paths = 0;
-        const newCurrentPath = [...currentPath];
 
         if (visited.has(node.id)) {
-            if (flag || node.leaf) {
+            if (exitCondidtion(node, flag)) {
                 return paths;
             }
 
@@ -72,20 +50,19 @@ export default class Day12Challenge extends AdventCommand<Input> {
             visited.add(node.id);
         }
 
-        newCurrentPath.push(node.id);
-
+        const newCurrentPath = [...currentPath, node.id];
         for (const link of node.links) {
-            paths += link === 'end' ? 1 : this.walk2(input, input.get(link), newCurrentPath, new Set(visited), flag);
+            paths += link === 'end' ? 1 : this.walk(input, input.get(link), exitCondidtion, newCurrentPath, new Set(visited), flag);
         }
 
         return paths;
     }
 
     protected part1(input: Input): number {
-        return this.walk1(input, input.get('start'));
+        return this.walk(input, input.get('start'), () => true);
     }
 
     protected part2(input: Input): number {
-        return this.walk2(input, input.get('start'));
+        return this.walk(input, input.get('start'), ({ isLeaf }, flag) => flag || isLeaf);
     }
 }
